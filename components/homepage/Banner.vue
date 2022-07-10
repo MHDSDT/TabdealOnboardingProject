db07daaadb6d16e71b44b1e4df2e3c72e93a968f
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-10 20:47:53 +0430

    Edit background color and description color

--- components/homepage/Banner.vue
+++ components/homepage/Banner.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="bg-neutral-900 block py-24 w-full h-[400px]">
+  <section class="bg-[#1a1c1f] block py-24 w-full h-[400px]">
     <div class="container mx-auto w-7/12" dir="rtl">
       <div class="relative flex flex-col items-center justify-center space-y-6 text-white">
         <h1 class="font-medium text-center text-5xl ">صرافی ارز دیجیتال تبدیل</h1>
-        <p class="text-center text-gray-300 text-lg">
+        <p class="text-center text-[#cacccf] text-lg">
           خرید و فروش امن و آسان به‌روزترین ارزهای دیجیتال جهان
         </p>
         <div class="relative flex flex-row items-center justify-center">
