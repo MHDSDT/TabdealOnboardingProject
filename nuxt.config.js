155a05e956c7630ed3a295388f6baa255d14ce79
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-10 17:09:17 +0430

    Change page title and language

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'TabdealOnboardingProject',
+    title: 'Tabdeal Onboarding Project',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'fa'
     },
     meta: [
       { charset: 'utf-8' },
