a7923546409251983c42ecb515d701a4af1b714d
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-11 10:33:54 +0430

    Add initial Header to Homepage

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Top Bar -->
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
