1091618946ff686647b0ef74e246f7e7a600738f
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-11 18:18:12 +0430

    Correct space between elements of CopyRight.vue

--- components/CopyRight.vue
+++ components/CopyRight.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer footer-center p-4 bg-[#1a1c1f] text-base-content text-[#cacccf]" dir="rtl">
-    <div>
-      <p>© تمام حقوق مادی و معنوی این سایت متعلق به تبدیل است.</p>
+    <div class="flex flex-row">
+      <p class="ml-4">© تمام حقوق مادی و معنوی این سایت متعلق به تبدیل است.</p>
       <a href="https://tabdeal.org/termsconditions/">
         قوانین و مقررات
       </a>
