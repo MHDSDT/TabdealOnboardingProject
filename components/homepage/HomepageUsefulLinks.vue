8c920071e0ab3512952579229922c5181682375f
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-11 17:34:16 +0430

    Add HomepageUsefulLinks.vue

---
+++ components/homepage/HomepageUsefulLinks.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hero py-16 lg:py-24 bg-white" dir="rtl">
+    <div class="hero-content flex flex-col lg:flex-row">
+      <a v-for="item in pros" :key="pros.title"
+           class="w-full flex flex-col text-black hover:shadow-xl hover:rounded-lg space-y-2 p-4 items-start hover:cursor-pointer"
+          @click="item">
+        <div class="flex flex-row self-center w-max h-max">
+          <img class="object-scale-down" :src="item.iconPath"
+               :alt="item.iconPath.split('/')[item.iconPath.split('/').length - 1].split('.')[0]"/>
+        </div>
+        <h1 class="text-lg hover:text-[#e0ac0c]">{{ item.title }}</h1>
+        <p class="text-sm">{{ item.description }}</p>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomepageUsefulLinks",
+  data() {
+    return {
+      pros: [
+        {
+          title: 'راهنمای گام به گام',
+          description: 'هر آنچه درباره استفاده از خدمات تبدیل باید بدانید را در اینجا بیابید.',
+          iconPath: 'https://tabdeal.org/images/homepage/new-content/useful-links-help-icon.svg',
+          link: 'https://help.tabdeal.org/'
+        },
+        {
+          title: 'آکادمی تبدیل',
+          description: 'راهنمای خرید و فروش ارز‌های دیجیتال همراه با آموزش‌های ویدیویی معامله‌گری',
+          iconPath: 'https://tabdeal.org/images/homepage/new-content/useful-links-blog-icon.svg',
+          link: 'https://academy.tabdeal.org/'
+        },
+        {
+          title: 'سوالات متداول',
+          description: 'در این بخش می‌توانید پاسخ سوالات متداولی را که ممکن است برای شما پیش بیاید، مشاهده کنید.',
+          iconPath: 'https://tabdeal.org/images/homepage/new-content/useful-links-faq-icon.svg',
+          link: 'https://tabdeal.org/help/faq'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <PageInfo />
     <ProsSection />
     <ApplicationPresentation />
+    <HomepageUsefulLinks />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Banner from '~/components/homepage/Banner';
 import PageInfo from "@/components/homepage/PageInfo";
 import ProsSection from "@/components/homepage/ProsSection";
 import ApplicationPresentation from "@/components/homepage/ApplicationPresentation";
+import HomepageUsefulLinks from "@/components/homepage/HomepageUsefulLinks";
 
 export default {
   name: 'HomePage',
@@ -19,7 +21,8 @@ export default {
     Banner,
     PageInfo,
     ProsSection,
-    ApplicationPresentation
+    ApplicationPresentation,
+    HomepageUsefulLinks
   },
 }
 </script>
