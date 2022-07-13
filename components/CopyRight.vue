d0537be667b7ff86b415051ba6e7c3d0273fc335
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-13 15:51:44 +0430

    Add divider in CopyRight.vue

--- components/CopyRight.vue
+++ components/CopyRight.vue
@@ -3,7 +3,8 @@
     class="footer footer-center p-4 bg-[#1a1c1f] text-base-content text-[#cacccf]"
   >
     <div class="flex flex-row">
-      <p class="ml-4">© تمام حقوق مادی و معنوی این سایت متعلق به تبدیل است.</p>
+      <p>© تمام حقوق مادی و معنوی این سایت متعلق به تبدیل است.</p>
+      <div class="divider divider-horizontal -mx-1"></div>
       <a href="https://tabdeal.org/termsconditions/"> قوانین و مقررات </a>
     </div>
   </footer>
