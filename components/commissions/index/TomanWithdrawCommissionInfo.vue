63f73ec47eb47e3095dc250efa7eb3b5662db7ad
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2022-07-25 09:46:24 +0430

    Complete commissions/index page

---
+++ components/commissions/index/TomanWithdrawCommissionInfo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex flex-col lg:w-4/5 gap-8 text-gray-600">
+    <h1 class="font-bold text-2xl text-black mr-2">کارمزد برداشت تومانی</h1>
+    <p class="mr-2">
+      کارمزد پرداختی توسط درگاه پرداخت، به‌سبب هزینه‌های بانکی، مطابق جدول زیر
+      دریافت می‌شود و تبدیل مستقیماً از کاربران، کارمزدی بابت برداشت تومانی
+      دریافت نمی‌نماید.
+    </p>
+    <div class="flex flex-col gap-4 rounded-lg shadow-xl bg-white p-8">
+      <div>
+        <div
+          class="flex bg-gray-200 rounded-lg font-bold text-xs text-gray-400 p-4"
+        >
+          <div
+            v-for="(tableHeader, index) in tableHeaders"
+            :class="colsPortion[index]"
+          >
+            {{ tableHeader }}
+          </div>
+        </div>
+        <div>
+          <div
+            v-for="tableRow in tableRows"
+            class="border-b p-4 flex text-xs sm:text-base"
+          >
+            <div :class="colsPortion[0]">{{ tableRow[0] }}</div>
+            <div :class="colsPortion[1]">{{ tableRow[1] }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TomanWithdrawCommissionInfo",
+  data() {
+    return {
+      tableHeaders: ["میزان برداشت تومانی", "کارمزد برداشت"],
+      colsPortion: ["w-2/3", "w-1/3"],
+      tableRows: [
+        ["کمتر از ۴۵۰ هزار تومان", "یک درصد از مبلغ برداشتی"],
+        ["بین ۴۵۰ هزار تومان و ۵۰ میلیون تومان", "۴۵۰۰ تومان"],
+        ["بیش از ۵۰ میلیون تومان", "۴۵۰۰ تومان به‌ازای هر ۵۰ میلیون تومان"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
+
+کم‌تر از 450 هزار تومان یک درصد از مبلغ برداشتی بین 450 هزار تومان و 50 میلیون
+تومان تومان 4500 بیش از 50 میلیون تومان 4500 تومان به‌ازای هر 50 میلیون تومان
--- pages/commissions/index.vue
+++ pages/commissions/index.vue
@@ -11,23 +11,71 @@
       <CommissionGeneralInfo />
       <CommissionIRTMarketsInfo />
       <SwapCommissionInfo />
+
+      <div class="lg:w-4/5 space-y-4 text-gray-600">
+        <h1 class="font-bold text-2xl text-black">
+          کارمزد برداشت و واریز ارز دیجیتال (کوین)
+        </h1>
+        <p class="text-justify">
+          کارمزد برداشت یا واریز ارزهای دیجیتال، به نوع ارز موردنظر و شبکهٔ
+          بلاک‌چین بستگی دارد. این کارمزد مربوط به تراکنش‌های درون‌شبکه‌ای است و
+          تبدیل در تعیین آن‌ها نقشی ندارد. این کارمزدها را به تفکیک هر کوین
+          می‌توانید در لینک زیر مشاهده نمایید.
+        </p>
+        <NuxtLink
+          to="/commissions/cryptofee"
+          class="btn btn-ghost btn-sm border border-gray-500 rounded font-medium text-xs text-gray-400"
+        >
+          جزئیات کارمزد برداشت ارز دیجیتال
+          <!--          <TIcon right>{{ mdiChevronLeft }}</TIcon>-->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            class="fill-gray-400 mr-1 stroke-0"
+          >
+            <path :d="mdiChevronLeft"></path>
+          </svg>
+        </NuxtLink>
+      </div>
+
+      <div class="lg:w-4/5 space-y-4 text-gray-600">
+        <h1 class="font-bold text-2xl text-black">کارمزد واریز تومانی</h1>
+        <p class="text-justify">
+          تبدیل بابت واریز تومانی، از کاربران کارمزدی دریافت نمی‌کند. صرفاً
+          امکان دارد کارمزد واریز توسط پرداخت‌یار دریافت شود.
+        </p>
+      </div>
+
+      <TomanWithdrawCommissionInfo />
+
+      <!-- TODO: TradeNowSection -->
     </section>
   </div>
 </template>
 
 <script>
+import { mdiChevronLeft } from "@mdi/js";
 import LightBanner from "@/components/general/LightBanner";
 import CommissionGeneralInfo from "@/components/commissions/index/CommissionGeneralInfo";
 import CommissionIRTMarketsInfo from "@/components/commissions/index/CommissionIRTMarketsInfo";
 import SwapCommissionInfo from "@/components/commissions/index/SwapCommissionInfo";
+import TomanWithdrawCommissionInfo from "@/components/commissions/index/TomanWithdrawCommissionInfo";
 
 export default {
   name: "index",
+  data() {
+    return {
+      mdiChevronLeft,
+    };
+  },
   components: {
     LightBanner,
     CommissionGeneralInfo,
     CommissionIRTMarketsInfo,
     SwapCommissionInfo,
+    TomanWithdrawCommissionInfo,
   },
 };
 </script>
